7b2ba96375af60975651816917da2f891e8ca7b8
ADOPALE <dsi@adopale.com>
2026-03-11 11:18:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 
 # Import des fonctions modules (vérifiez que vos fichiers ont ces noms dans /modules)
 from modules.Import import show_import
-"from modules.dataViz import show_volumes, show_biologie
-"from modules.paramSim import show_simulation
+#from modules.dataViz import show_volumes, show_biologie
+#from modules.paramSim import show_simulation
 # Note : Les résultats ne s'affichent que si une condition est remplie
 
 # CONFIGURATION PAGE
